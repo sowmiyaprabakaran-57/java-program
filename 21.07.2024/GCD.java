e5339c1c30505530cb7e659bbd0b2fc87e0175d0
sowmiyaprabakaran-57 <168167244+sowmiyaprabakaran-57@users.noreply.github.com>
2024-07-21 22:54:21 +0530

    Create GCD.java

---
+++ 21.07.2024/GCD.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class GCD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        int gcd = findGCD(num1, num2);
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + gcd);
+    }
+    public static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return findGCD(b, a % b);
+    }
+}
